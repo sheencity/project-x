4d1a3d1f0debf20d6f77507749fb0746551f106e
wujiale <wujiale0907@qq.com>
2020-02-06 16:33:05 +0800

    chore: add on date change event

--- src/pages/travelmap/unimap/unimap.vue
+++ src/pages/travelmap/unimap/unimap.vue
@@ -1,11 +1,24 @@
 <template>
 	<view>
-		<page-head :title="title"></page-head>
+		<!-- <page-head :title="title"></page-head> -->
 		<view class="uni-padding-wrap uni-common-mt">
-		    <view>
-                <picker mode="date"
-            </view>
             <view class="uni-flex uni-column">
+                <view class="uni-list">
+                    <view class="uni-list-cell">
+                        <view class="uni-list-cell-left">
+                            日期
+                        </view>
+                        <view class="uni-list-cell-db">
+                            <picker mode="date" 
+                                :value="date" 
+                                :start="startDate" 
+                                :end="endDate" 
+                                @change="bindDateChange">
+                                <view class="uni-input">{{date}}</view>
+                            </picker>
+                        </view>
+                    </view>
+                </view>
                 <view >
                     <map 
                         style="width: 750rpx; height: 750rpx;" 
@@ -21,163 +34,86 @@
 </template>
 <script>
     import MapLoader from 'assets/amap.js'
+
+    function getDate(type) {
+		const date = new Date();
+	
+		let year = date.getFullYear();
+		let month = date.getMonth() + 1;
+		let day = date.getDate();
+	
+		if (type === 'start') {
+            year = year;
+            month = 1;
+            day = 1;
+		} else if (type === 'end') {
+			year = year + 2;
+		}
+		month = month > 9 ? month : '0' + month;;
+		day = day > 9 ? day : '0' + day;
     
+		return `${year}-${month}-${day}`;
+	}
+
 	export default {
 		data() {
     		return {
                 title: 'map',
-                polylines: [
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 117.200983,
-                                "longitude": 39.084158
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 114.530235,
-                                "longitude": 38.037433
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 112.562678,
-                                "longitude": 37.873499
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 111.76629,
-                                "longitude": 40.81739
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 123.431382,
-                                "longitude": 41.836175
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 125.32568,
-                                "longitude": 43.897016
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 126.661665,
-                                "longitude": 45.742366
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 121.473662,
-                                "longitude": 31.230372
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 118.762765,
-                                "longitude": 32.060875
-                            }
-                        ]
-                    },
-                    {
-                        "points": [
-                            {
-                                "latitude": 116.407394,
-                                "longitude": 39.904211
-                            },
-                            {
-                                "latitude": 120.152585,
-                                "longitude": 30.266597
-                            }
-                        ]
-                    }
-                ],
-                latitude:39.92,
-                longitude: 116.46,
+                polylines: [],
+            　　latitude: 40.013305,
+            　　longitude: 118.685713,
                 covers:[
                     {
                         id: 1,
-                        latitude:39.92,
-                        longitude: 116.46,
+                　　latitude: 40.013305,
+                　　longitude: 118.685713,
                         iconPath:'',
                         title: 'test'
                     }
-                ]
+                ],
+                date: '2020-01-20'
 			}
         },
-        methods:{
-            evalLines(){
-                console.log('eval lines');
-                console.log(CityLines.lines);
-                return CityLines.lines.map( x => {
-                    return {
-                        points : x.line.map(point=>{
-                            const datas = cur.split(',', 2);
-                            return {
-                                latitude: datas[0],
-                                longitude: datas[1],
-                            }
-                        }) 
+        computed: {
+            startDate(){
+                return getDate('start');
+            },
+            endDate() {
+                return getDate('end');
+            },
+            // polylines(){
+                
+            //     uni.request({
+            //         url:'https://service-9nqsnd2b-1301203847.bj.apigw.tencentcs.com/release/js_func',
+
+            //         success: (data, status)=>{
+            //             console.log('requrest status ' + status);
+            //             // console.log(data);
+            //             this.polylines
+            //     }});
+            // }
+        },
+        methods: {
+            bindDateChange: function(e) {
+                this.date = e.target.value;
+                let that = this;
+                console.log('date changed');
+
+                uni.request({
+                    url:'https://service-9nqsnd2b-1301203847.bj.apigw.tencentcs.com/release/js_func',
+                    data:{
+                        date:that.date
                     }
+                }).then(data=>{
+                    var [error, res]  = data;
+                    that.polylines = res.data.body;
+                    console.log(res.data);
+                }).catch(error=>{
+                    console.log(error);
                 });
-            }
+            },
+            
         }
-		
+       		
 	}
 </script>
